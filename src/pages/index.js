57b01d0e27f2bd88a6af6cf3b583c6a440f4903f
Zed Nguyen <zednguyen04@gmail.com>
2021-04-07 16:10:09 +0700

    Installing and configuring Tailwind + Twin Bable Macro and Emotion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import tw, { styled } from "twin.macro"
+
+const Button = styled.button`
+  ${tw`bg-blue-500 hover:bg-blue-800 text-white p-2 rounded`}
+`
+// or use shorthand version
+//
+const ButtonPrivate = tw.button`
+  bg-green-500 hover:bg-blue-900 text-white p-2 rounded
+`
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <p>Hello world!</p>
+      <Button>Click me</Button>
+      <ButtonPrivate>Button Private</ButtonPrivate>
+    </div>
+  )
 }
